a8c44c7233891ee625dd71e0e85c272b66c299e0
darioargel <aalvarez4713@ucq.edu.mx>
2025-06-26 00:29:53 -0600

    Add multiplication function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,15 @@
+# calculator.py
+def multiply(n1, n2):
+    return n1 * n2
+
 def calculator():
-    print("Calculadora simple")
+    print("Calculadora: Multiplicación")
+    try:
+        n1 = float(input("Enter first number: "))
+        n2 = float(input("Enter second number: "))
+        print(f"{n1} * {n2} = {multiply(n1, n2)}")
+    except ValueError:
+        print("Invalid input: Please enter valid numbers")
 
 if __name__ == "__main__":
     calculator()
