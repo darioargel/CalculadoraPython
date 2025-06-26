a20008671b432ea8db48bdcdcfcd889a6f7c9d3c
darioargel <aalvarez4713@ucq.edu.mx>
2025-06-26 00:26:15 -0600

    Add addition function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,14 @@
+def add(n1, n2):
+    return n1 + n2
+
 def calculator():
-    print("Calculadora simple")
+    print("Calculadora: Suma")
+    try:
+        n1 = float(input("Enter first number: "))
+        n2 = float(input("Enter second number: "))
+        print(f"{n1} + {n2} = {add(n1, n2)}")
+    except ValueError:
+        print("Invalid input: Please enter valid numbers")
 
 if __name__ == "__main__":
     calculator()
